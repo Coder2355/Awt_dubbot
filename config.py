55acd3fd7c8c5706540158627f943dafa332e5b5
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-28 07:40:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ class Config:
     API_ID = os.environ.get("API_ID", "21740783")
     API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-    DOWNLOAD_DIR = "./downloads/"
-    UPLOAD_DIR = "./uploads/"
-    USER_DATA_FILE = "./user_data.json"
+    
+    VOICE_DIR = os.getenv('VOICE_DIR', 'voices')
+    DOWNLOAD_DIR = os.getenv('DOWNLOAD_DIR', 'downloads')
+
+# File paths
+    VOICE_MAP_FILE = os.getenv('VOICE_MAP_FILE', 'voice_map.json')
