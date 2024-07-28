eb9368a600bb292a0d610062adc7982552bdaf03
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-28 06:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "your_api_id")
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+    API_ID = os.environ.get("API_ID", "21740783")
+    API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
     DOWNLOAD_DIR = "./downloads/"
     UPLOAD_DIR = "./uploads/"
     USER_DATA_FILE = "./user_data.json"
