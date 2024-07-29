5a3383e20e1fa857d625f22901a353c410294ec1
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-29 10:54:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = os.environ.get("API_ID", "21740783")
 API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
     
-WATERMARK_IMAGE_PATH = "0"  # Path to your watermark image
+FFMPEG_PATH = 'ffmpeg'  # Adjust this path if FFmpeg is not in your PATH
 UPLOAD_FOLDER = 'uploads'
-PROCESSED_FOLDER = 'processed'
-ALLOWED_EXTENSIONS = {'mp4', 'mov', 'avi'}
+DUBBED_FOLDER = 'dubbed'
+PORT = 8080
