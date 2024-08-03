fe77baae9366789c1cf9e2a285ff97f8be26ac07
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-03 10:02:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 API_ID = os.environ.get("API_ID", "21740783")
 API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     
 FFMPEG_PATH = 'ffmpeg'  # Adjust this path if FFmpeg is not in your PATH
 UPLOAD_FOLDER = 'uploads'
 DUBBED_FOLDER = 'dubbed'
 PORT = 8080
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = 'path/to/your/google-cloud-speech-credentials.json'
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = '
